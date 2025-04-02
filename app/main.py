6176232265fe36e25e4a67956c5442a42e91e411
Asad Ali <asadali.asif.1@gmail.com>
2025-04-02 21:42:00 +0100

    Stage: Extract URL Path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import socket  # noqa: F401
 
+CRLF = "\r\n"
+
+response_statuses = {
+    "OK": "200 OK",
+    "NOT FOUND": "404 NOT FOUND"
+}
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,10 +14,20 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=False) # CHANGE REUSEPORT TO TRUE BEFORE SUBMISSION
-    conn, addr = server_socket.accept() # wait for client
 
+    
+    conn, addr = server_socket.accept() # wait for client
     print(f"Connected by {addr}")
-    message = "HTTP/1.1 200 OK\r\n\r\n"
+    data = conn.recv(1024) # Read upto 1024 bytes
+    requested_resource = data.decode("utf-8").split(CRLF)[0].split(" ")[1]
+
+    message = "HTTP/1.1 "
+    if requested_resource == "/":
+        message += response_statuses["OK"]
+    else:
+        message += response_statuses["NOT FOUND"]
+
+    message += CRLF + CRLF
     conn.send(message.encode())
 
 
